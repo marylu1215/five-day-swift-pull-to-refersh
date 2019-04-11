8c9c6feed8aa160eafd2ceadccf345e03c0212f9
jun xie <iumnyo@163.com>
2019-04-11 15:35:03 +0800

    Initial Commit

---
+++ five-day-swift mjRefresh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  five-day-swift mjRefresh
+//
+//  Created by MacBook Pro on 2019/4/11.
+//  Copyright © 2019年 MacBookPro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ five-day-swift mjRefreshTests/five_day_swift_mjRefreshTests.swift
@@ -0,0 +1,34 @@
+//
+//  five_day_swift_mjRefreshTests.swift
+//  five-day-swift mjRefreshTests
+//
+//  Created by MacBook Pro on 2019/4/11.
+//  Copyright © 2019年 MacBookPro. All rights reserved.
+//
+
+import XCTest
+@testable import five_day_swift_mjRefresh
+
+class five_day_swift_mjRefreshTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
